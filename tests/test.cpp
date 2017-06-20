de84096c51f886dc0ad7e0f317a8347522c92d01
Alex Bleasdale <ableasdale@marklogic.com>
2017-06-20 12:30:13 +0100

    Added logging and removed (commented out) slfv until i figure out how to fix the linking on OS X

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,11 +1,16 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "easylogging++.h"
+
+INITIALIZE_EASYLOGGINGPP
 
 unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
 }
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
+
+    LOG(INFO) << "My first info log using default logger";
 
     REQUIRE( Factorial(1) == 1 );
 	REQUIRE( Factorial(2) == 2 );
