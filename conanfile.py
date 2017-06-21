7c62ef36c478c192240b9770425ea7d7d71de319
Alex Bleasdale <ableasdale@marklogic.com>
2017-06-21 09:49:09 +0100

    Still battling with OS X dyld issues :(

--- conanfile.py
+++ conanfile.py
@@ -6,11 +6,22 @@ class MySimpleProject(ConanFile):
     name = "Learning Conan"
     version = "0.1"
     settings = "os", "compiler", "build_type", "arch"
-    # requires = "catch/1.5.0@TyRoXx/stable", "backward/1.3.0@Manu343726/testing", "Poco/1.7.2@lasote/stable", "Boost/1.60.0@lasote/stable", "sfml/2.3.2@cpbotha/stable", "easyloggingpp/9.80@memsharded/testing"
     requires = ("catch/1.5.0@TyRoXx/stable",
         "backward/1.3.0@Manu343726/testing",
-        "Poco/1.7.2@lasote/stable",
-        "Boost/1.60.0@lasote/stable",
-        "easyloggingpp/9.80@memsharded/testing")
-        # "CppRestSdk/2.8.0@ViaviSolutions/stable")
+        "Poco/1.7.5@lasote/stable",
+        "Boost/1.62.0@lasote/stable",
+        "easyloggingpp/9.80@memsharded/testing",
+        "CppRestSdk/2.9.0@BleuGamer/stable")
     generators = "cmake"
+
+    def imports(self):
+        self.copy("*.dll", dst="bin", src="bin") # From bin to bin
+        self.copy("*.dylib*", dst="bin", src="lib") # From lib to bin
+     #   self.copy("*.dylib*", dst="bin", src="lib") # From lib to bin
+
+
+
+    # default_options = "Poco:shared=True", "OpenSSL:shared=True"
+    # requires = "catch/1.5.0@TyRoXx/stable", "backward/1.3.0@Manu343726/testing", "Poco/1.7.2@lasote/stable", "Boost/1.60.0@lasote/stable", "sfml/2.3.2@cpbotha/stable", "easyloggingpp/9.80@memsharded/testing"
+
+# DYLD_LIBRARY_PATH
--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
+#!/usr/bin/env bash
 rm -rf build
 mkdir build && cd build
+# export DYLD_LIBRARY_PATH=.:$DYLD_LIBRARY_PATH
+# export DYLD_FALLBACK_LIBRARY_PATH=.:$DYLD_FALLBACK_LIBRARY_PATH
 conan install .. -s compiler=apple-clang -s compiler.version=8.1 -s compiler.libcxx=libc++ --build=missing
 cmake ..
 cmake --build .
